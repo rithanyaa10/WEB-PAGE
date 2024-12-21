d3b35c02ae9ae93a8188fa979f43eb2d9eebb8fb
rithanyaa10 <rithanyaa.2453037@srec.ac.in>
2024-12-21 18:21:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault(); // Prevent form submission
 
     const email = document.getElementById('email').value;
-    const rollno = document.getElementById('rollno').value;
+    const roll no = document.getElementById('roll no').value;
     const message = document.getElementById('message');
 
     // Simple validation (you can expand this)
-    if (email === "test@example.com" && rollno === "123456") {
+    if (email === "test@example.com" && roll no === "123456") {
         message.style.color = "green";
         message.textContent = "Login successful!";
     } else {
