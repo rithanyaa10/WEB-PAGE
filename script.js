a9eab23f8adf9791342fd186f0b9ccf1f85f5d1b
rithanyaa10 <rithanyaa.2453037@srec.ac.in>
2024-12-21 21:35:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+document.getElementById('loginForm').addEventListener('submit', function(event)
+{
+    event.preventDefault(); 
 
     const email = document.getElementById('email').value;
     const roll no = document.getElementById('roll no').value;
     const message = document.getElementById('message');
-
-    // Simple validation (you can expand this)
     if (email === "test@example.com" && roll no === "123456") 
      {
         message.style.color = "green";
@@ -14,6 +13,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     else 
      {
         message.style.color = "red";
-        message.textContent = "Invalid email or password.";
+        message.textContent = "Invalid email or roll no.";
      }
 });
